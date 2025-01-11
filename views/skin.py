1278b97f8357fbcfa5c48f6e0268a09857f47e33
Matahari-Masalalu <150545655+Matahari-Masalalu@users.noreply.github.com>
2025-01-12 03:55:24 +0700

    Update skin.py

--- views/skin.py
+++ views/skin.py
@@ -1,100 +1,66 @@
 import streamlit as st
-import numpy as np
-from PIL import Image, ImageOps
-import tensorflow as tf
+from classes.skin_disease_classifier import SkinDiseaseClassifier
+from PIL import Image
 
-# Load labels
-def load_labels(label_path):
-    with open(label_path, 'r') as file:
-        labels = [line.strip().split(' ', 1)[1] for line in file.readlines()]
-    return labels
+class SkinPage:
+    def __init__(self):
+        self.title = "Klasifikasi Penyakit Kulit"
 
-# Load TFLite model
-@st.cache_resource
-def load_model(model_path):
-    interpreter = tf.lite.Interpreter(model_path=model_path)
-    interpreter.allocate_tensors()
-    return interpreter
+    def display(self):
+        st.title(self.title)
 
-# Classify image using TFLite model
-def classify_image(image, interpreter, class_names, img_size=(450, 450)):
-    image = ImageOps.fit(image, img_size, Image.Resampling.LANCZOS)  # Resize image
-    image_array = np.asarray(image) / 255.0  # Normalize image
-    data = np.expand_dims(image_array, axis=0).astype(np.float32)  # Expand dimensions for the model input
+        if 'page' not in st.session_state:
+            st.session_state.page = 'info'
 
-    # Get input and output tensors
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
+        col1, col2 = st.columns(2)
 
-    # Set the tensor to the input data
-    interpreter.set_tensor(input_details[0]['index'], data)
-    interpreter.invoke()  # Run inference
+        with col1:
+            if st.button("Informasi tentang Penyakit Kulit", key="info_button"):
+                st.session_state.page = "info"
 
-    # Get the output
-    prediction = interpreter.get_tensor(output_details[0]['index'])
-    top_prob_index = np.argmax(prediction[0])
-    confidence_score = float(prediction[0][top_prob_index])
-    return class_names[top_prob_index], confidence_score
+        with col2:
+            if st.button("Prediksi Gambar Penyakit Kulit", key="predict_button"):
+                st.session_state.page = "predict"
 
-# Streamlit UI setup
-st.title("Klasifikasi Penyakit Kulit dengan Machine Learning")
+        if st.session_state.page == "info":
+            self.display_info()
+        elif st.session_state.page == "predict":
+            self.predict_skin_disease()
 
-# Default state if this is the first time running
-if 'page' not in st.session_state:
-    st.session_state.page = 'info'  # Default to information page
+    def display_info(self):
+        st.header("Informasi tentang Penyakit Kulit 🩺")
+        st.write("""
+            Teknologi machine learning dapat digunakan untuk mendiagnosis dan memprediksi penyakit kulit berdasarkan 
+            gambar dan data pasien. Beberapa penyakit kulit yang umum diprediksi meliputi:
+            - **Actinic keratosis**
+            - **Atopic Dermatitis**
+            - **Benign keratosis**
+            - **Dermatofibroma**
+            - **Melanocytic nevus**
+            - **Melanoma**
+            - **Squamous cell carcinoma**
+            - **Tinea Ringworm Candidiasis**
+            - **Vascular lesion**
+            
+            Model machine learning dapat dilatih menggunakan dataset gambar kulit yang telah diberi label untuk 
+            mengenali pola dan gejala yang terkait dengan penyakit tertentu. Dengan demikian, pasien dapat 
+            mendapatkan diagnosis awal yang lebih cepat dan akurat.
 
-# Create layout for buttons
-col1, col2 = st.columns(2)
+            Penting untuk diingat bahwa meskipun teknologi ini menjanjikan, konsultasi dengan dokter kulit tetap 
+            diperlukan untuk diagnosis dan perawatan yang tepat.
+        """)
 
-with col1:
-    if st.button("Informasi tentang Penyakit Kulit", key="info_button"):
-        st.session_state.page = "info"
+    def predict_skin_disease(self):
+        model_path = 'model/model_quantized.tflite'
+        label_path = 'model/labels.txt'
+        classifier = SkinDiseaseClassifier(model_path, label_path)
 
-with col2:
-    if st.button("Prediksi Gambar Penyakit Kulit", key="predict_button"):
-        st.session_state.page = "predict"
+        uploaded_file = st.file_uploader("Pilih gambar...", type=["jpg", "png", "jpeg"])
 
-# Render the corresponding content based on the state
-if st.session_state.page == "info":
-    st.header("Informasi tentang Penyakit Kulit 🩺")
-    st.write("""
-        Teknologi machine learning dapat digunakan untuk mendiagnosis dan memprediksi penyakit kulit berdasarkan 
-        gambar dan data pasien. Beberapa penyakit kulit yang umum diprediksi meliputi:
-        - **Actinic keratosis**
-        - **Atopic Dermatitis**
-        - **Benign keratosis**
-        - **Dermatofibroma**
-        - **Melanocytic nevus**
-        - **Melanoma**
-        - **Squamous cell carcinoma**
-        - **Tinea Ringworm Candidiasis**
-        - **Vascular lesion**
-        
-        Model machine learning dapat dilatih menggunakan dataset gambar kulit yang telah diberi label untuk 
-        mengenali pola dan gejala yang terkait dengan penyakit tertentu. Dengan demikian, pasien dapat 
-        mendapatkan diagnosis awal yang lebih cepat dan akurat.
-        
-        Penting untuk diingat bahwa meskipun teknologi ini menjanjikan, konsultasi dengan dokter kulit tetap 
-        diperlukan untuk diagnosis dan perawatan yang tepat.
-    """)
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+            class_name, confidence_score = classifier.classify_image(image)
 
-elif st.session_state.page == "predict":
-    # Load model and labels
-    model_path = 'model/model_quantized.tflite'
-    label_path = 'model/labels.txt'
-    interpreter = load_model(model_path)
-    class_names = load_labels(label_path)
-
-    # Upload image
-    uploaded_file = st.file_uploader("Pilih gambar...", type=["jpg", "png", "jpeg"])
-
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        
-        # Classify the image
-        class_name, confidence_score = classify_image(image, interpreter, class_names)
-
-        # Display the classification result above the image
-        st.write(f"Prediksi Kelas: **{ class_name}**")
-        st.write(f"Skor Kepercayaan: **{confidence_score:.2f}**")
-        st.image(image, caption='Gambar yang Diupload', width=300)  # Set a maximum width for the image
+            st.write(f"Prediksi Kelas: **{class_name}**")
+            st.write(f"Skor Kepercayaan: **{confidence_score:.2f}**")
+            st.image(image, caption='Gambar yang Diupload', width=300)
