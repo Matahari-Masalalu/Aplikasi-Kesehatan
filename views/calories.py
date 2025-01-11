43e2d8195b5fbd269b0ffe7b4e00daebda168a32
Matahari-Masalalu <150545655+Matahari-Masalalu@users.noreply.github.com>
2025-01-12 03:56:27 +0700

    Update calories.py

--- views/calories.py
+++ views/calories.py
@@ -1,106 +1,85 @@
 import streamlit as st
+from classes.user import User
+from classes.calorie_calculator import CalorieCalculator
 
-class User:
-    def __init__(self, age, height, weight, gender, activity_level):
-        self.age = age
-        self.height = height
-        self.weight = weight
-        self.gender = gender
-        self.activity_level = activity_level
+class CaloriesPage:
+    def __init__(self):
+        self.title = "Kalkulator Kebutuhan Kalori"
 
-class CalorieCalculator:
-    def __init__(self, user):
-        self.user = user
+    def display(self):
+        st.title(self.title)
 
-    def calculate_bmr(self):
-        if self.user.gender == "Laki-laki":
-            return 10 * self.user.weight + 6.25 * self.user.height - 5 * self.user.age + 5
-        else:
-            return 10 * self.user.weight + 6.25 * self.user.height - 5 * self.user.age - 161
+        if 'page' not in st.session_state:
+            st.session_state.page = 'info'
 
-    def calculate_daily_calories(self):
-        bmr = self.calculate_bmr()
-        activity_multiplier = {
-            "Sedentari (tidak aktif)": 1.2,
-            "Ringan (olahraga ringan 1-3 hari/minggu)": 1.375,
-            "Sedang (olahraga sedang 3-5 hari/minggu)": 1.55,
-            "Tinggi (olahraga berat 6-7 hari/minggu)": 1.725,
-        }
-        return bmr * activity_multiplier[self.user.activity_level]
+        col1, col2 = st.columns(2)
 
-# Set the title of the app
-st.title("Kalkulator Kebutuhan Kalori Harian dan Informasi Kesehatan")
+        with col1:
+            if st.button("Informasi tentang Kebutuhan Kalori", key="info_button"):
+                st.session_state.page = "info"
 
-# Default state if first time running
-if 'page' not in st.session_state:
-    st.session_state.page = 'info'  # Default to information page
+        with col2:
+            if st.button("Hitung Kebutuhan Kalori", key="calorie_button"):
+                st.session_state.page = "calorie"
 
-# Create layout for buttons
-col1, col2 = st.columns(2)
+        if st.session_state.page == "calorie":
+            self.calculate_calories()
+        elif st.session_state.page == "info":
+            self.display_info()
 
-with col1:
-    if st.button("Informasi tentang Kebutuhan Kalori", key="info_button"):
-        st.session_state.page = "info"  # Change state to information page
+    def display_with_info(self):
+        self.display_info()
+        self.display()
 
-with col2:
-    if st.button("Hitung Kebutuhan Kalori", key="calorie_button"):
-        st.session_state.page = "calorie"  # Change state to calorie calculator page
+    def calculate_calories(self):
+        st.header("Hitung Kebutuhan Kalori Harian")
+        st.header("Masukkan Data Anda")
 
-# Render the corresponding content based on the state
-if st.session_state.page == "calorie":
-    st.header("Hitung Kebutuhan Kalori Harian")
+        age = st.number_input("Umur (tahun)", min_value=0, format="%d")
+        height = st.number_input("Tinggi Badan (cm)", min_value=0.0, format="%.2f")
+        weight = st.number_input("Berat Badan (kg)", min_value=0.0, format="%.2f")
+        gender = st.selectbox("Jenis Kelamin", ["Laki-laki", "Perempuan"])
+        activity_level = st.selectbox("Tingkat Aktivitas", ["Sedentari (tidak aktif)", 
+                                                              "Ringan (olahraga ringan 1-3 hari/minggu)", 
+                                                              "Sedang (olahraga sedang 3-5 hari/minggu)", 
+                                                              "Tinggi (olahraga berat 6-7 hari/minggu)"])
 
-    # Input data pengguna
-    st.header("Masukkan Data Anda")
+        if st.button("Hitung Kebutuhan Kalori Harian", key="calculate_calories"):
+            if weight > 0 and height > 0 and age > 0:
+                user = User(age, height, weight, gender, activity_level)
+                calculator = CalorieCalculator(user)
+                daily_calories = calculator.calculate_daily_calories()
 
-    # Input fields using columns for better layout
-    age = st.number_input("Umur (tahun)", min_value=0, format="%d")
-    height = st.number_input("Tinggi Badan (cm)", min_value=0.0, format="%.2f")
-    weight = st.number_input("Berat Badan (kg)", min_value=0.0, format="%.2f")
-    gender = st.selectbox("Jenis Kelamin", ["Laki-laki", "Perempuan"])
-    activity_level = st.selectbox("Tingkat Aktivitas", ["Sedentari (tidak aktif)", 
-                                                          "Ringan (olahraga ringan 1-3 hari/minggu)", 
-                                                          "Sedang (olahraga sedang 3-5 hari/minggu)", 
-                                                          "Tinggi (olahraga berat 6-7 hari/minggu)"])
+                st.subheader("Hasil")
+                st.write(f"Kebutuhan kalori harian Anda adalah: **{daily_calories:.2f} kalori**")
+            else:
+                st.warning("Silakan masukkan data yang valid.")
 
-    # Button to calculate daily calories
-    if st.button("Hitung Kebutuhan Kalori Harian", key="calculate_calories"):
-        if weight > 0 and height > 0 and age > 0:
-            user = User(age, height, weight, gender, activity_level)
-            calculator = CalorieCalculator(user)
-            daily_calories = calculator.calculate_daily_calories()
+    def display_info(self):
+        st.header("Informasi tentang Kebutuhan Kalori Harian")
+        st.write("""
+            Kebutuhan kalori harian bervariasi tergantung pada beberapa faktor termasuk umur, jenis kelamin, 
+            tinggi badan, berat badan, dan tingkat aktivitas. 
+            Penting untuk mengetahui kebutuhan kalori agar Anda dapat menjaga berat badan yang sehat dan 
+            menghindari masalah kesehatan yang terkait dengan asupan kalori yang tidak seimbang.
+            
+            Berikut adalah rumus untuk menghitung BMR (Basal Metabolic Rate):
+            
+            $$
+            BMR = 10 \times berat \, (kg) + 6.25 \times tinggi \, (cm) - 5 \times umur \, (tahun) + 5 \, (Laki-laki) \, atau \, -161 \, (Perempuan)
+            $$
+            
+            Setelah BMR dihitung, kali dengan faktor aktivitas untuk mendapatkan kebutuhan kalori harian:
+            - Sedentari (tidak aktif): BMR × 1.2
+            - Ringan (olahraga ringan/satu kali seminggu): BMR × 1.375
+            - Sedang (olahraga sedang/3-5 kali seminggu): BMR × 1.55
+            - Aktif (olahraga berat/6-7 kali seminggu): BMR × 1.725
+        """)
 
-            # Display results
-            st.subheader("Hasil")
-            st.write(f"Kebutuhan kalori harian Anda adalah: **{daily_calories:.2f} kalori**")
-        else:
-            st.warning("Silakan masukkan data yang valid.")
-
-elif st.session_state.page == "info":
-    st.header("Informasi tentang Kebutuhan Kalori Harian")
-    st.write("""
-        Kebutuhan kalori harian bervariasi tergantung pada beberapa faktor termasuk umur, jenis kelamin, 
-        tinggi badan, berat badan, dan tingkat aktivitas. 
-        Penting untuk mengetahui kebutuhan kalori agar Anda dapat menjaga berat badan yang sehat dan 
-        menghindari masalah kesehatan yang terkait dengan asupan kalori yang tidak seimbang.
-        
-        Berikut adalah rumus untuk menghitung BMR (Basal Metabolic Rate):
-        
-        $$
-        BMR = 10 \times berat \, (kg) + 6.25 \times tinggi \, (cm) - 5 \times umur \, (tahun) + 5 \, (Laki-laki) \, atau \, -161 \, (Perempuan)
-        $$
-        
-        Setelah BMR dihitung, kali dengan faktor aktivitas untuk mendapatkan kebutuhan kalori harian:
-        - Sedentari (tidak aktif): BMR × 1.2
-        - Ringan (olahraga ringan/satu kali seminggu): BMR × 1.375
-        - Sedang (olahraga sedang/3-5 kali seminggu): BMR × 1.55
-        - Aktif (olahraga berat/6-7 kali seminggu): BMR × 1.725
-    """)
-
-    st.subheader("Mengapa Penting untuk Menghitung Kebutuhan Kalori?")
-    st.write("""
-        Menghitung kebutuhan kalori dapat membantu Anda menentukan asupan kalori yang tepat untuk mencapai tujuan 
-        kesehatan Anda, baik itu untuk menurunkan berat badan, mempertahankan berat badan, atau menambah berat badan. 
-        Dengan pemahaman yang baik tentang kebutuhan kalori Anda, Anda dapat membuat pilihan makanan yang lebih sehat 
-        dan mengikuti pola hidup yang lebih aktif.
-    """)
+        st.subheader("Mengapa Penting untuk Menghitung Kebutuhan Kalori?")
+        st.write("""
+            Menghitung kebutuhan kalori dapat membantu Anda menentukan asupan kalori yang tepat untuk mencapai tujuan 
+            kesehatan Anda, baik itu untuk menurunkan berat badan, mempertahankan berat badan, atau menambah berat badan. 
+            Dengan pemahaman yang baik tentang kebutuhan kalori Anda, Anda dapat membuat pilihan makanan yang lebih sehat 
+            dan mengikuti pola hidup yang lebih aktif.
+        """)
